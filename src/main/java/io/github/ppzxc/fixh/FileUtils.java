44a5f2db9baa6f096056fbc753008446087d0324
ppzxc <cjh8487@naver.com>
2025-10-30 16:54:23 +0900

    2025-10-30 add Func In FileUtils.java

--- src/main/java/io/github/ppzxc/fixh/FileUtils.java
+++ src/main/java/io/github/ppzxc/fixh/FileUtils.java
@@ -1,6 +1,9 @@
 package io.github.ppzxc.fixh;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public final class FileUtils {
 
@@ -9,5 +12,9 @@ public final class FileUtils {
 
   public static String[] getFileNames(String path, String startsWith) {
     return new File(path).list((dir, name) -> name.startsWith(startsWith));
+  }
+
+  public static List<String> getFileNamesToList(String path, String startsWith) {
+    return Arrays.stream(getFileNames(path, startsWith)).collect(Collectors.toList());
   }
 }
--- src/test/java/io/github/ppzxc/fixh/FileUtilsTest.java
+++ src/test/java/io/github/ppzxc/fixh/FileUtilsTest.java
@@ -10,4 +10,9 @@ class FileUtilsTest {
   void should_find_file_names() {
     assertThat(FileUtils.getFileNames(PathUtils.getUserDir(), "README")).containsExactly("README.md");
   }
+
+  @Test
+  void should_find_file_names_to_list() {
+    assertThat(FileUtils.getFileNamesToList(PathUtils.getUserDir(), "README")).containsExactly("README.md");
+  }
 }
